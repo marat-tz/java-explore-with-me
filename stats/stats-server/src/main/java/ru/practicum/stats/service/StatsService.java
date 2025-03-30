590f073b50fcd1c71df7368f537727b0ba257539
Marat Tazhetdinov <unlim089@yandex.ru>
2025-03-30 23:17:10 +0300

    Fix codestyle

--- stats/stats-server/src/main/java/ru/practicum/stats/service/StatsService.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/service/StatsService.java
@@ -10,5 +10,6 @@ import java.util.List;
 public interface StatsService {
 
     void create(EndpointHitDtoRequest dto);
+
     List<ViewStatDtoResponse> findStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique);
 }
